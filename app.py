0328ccb86fd3eb2d15de2d035607f253bfa468cc
cavesdev <24899540+cavesdev@users.noreply.github.com>
2019-09-06 13:00:32 -0500

    added login and signup routes with sql queries

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
+import requests
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+KEY = 'yAcxcAMdMRIghLzrb86x5Q'
 
 app = Flask(__name__)
 
@@ -21,6 +24,33 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        hello = db.execute('SELECT * FROM users WHERE username = :username AND password = :password',
+                   {'username': username, 'password': password}).first()
+
+        return render_template('login.html')
+
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        db.execute('INSERT INTO users(username, password) VALUES (:username, :password)',
+                   {'username': username, 'password':password})
+        db.commit()
+        return render_template('login.html')
+    return render_template('signup.html')
